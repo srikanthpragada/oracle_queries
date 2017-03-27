8629236d7a6f5ada985bb8a632f160a20a0ba9a8
Srikanth Pragada <srikanthpragada@gmail.com>
2017-03-27 17:15:51 +0530

    Advanced Queries

--- more_queries.sql
+++ more_queries.sql
@@ -7,4 +7,17 @@ where 4 =
      from employees
      where salary > outer.salary)
      
-     
+-- display details of old jobs done by employees     
+select first_name, job_title, floor(months_between(end_date, start_date)),
+department_name, city
+from employees e join job_history jh using(employee_id) 
+join jobs j on (jh.job_id = j.job_id) 
+join departments d on (jh.department_id = d.department_id)
+join locations using (location_id)
+order by 1
+
+--  Display names of employees who draw more than their managers
+select e.first_name
+from employees e join employees m
+on (e.manager_id = m.employee_id)
+where  e.salary > m.salary
